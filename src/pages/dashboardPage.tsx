7d2898a0f2fe9cede722aed86011f4ceb1103f5f
jose-henrique <clanosbrasil2003@gmail.com>
2020-11-24 10:02:05 -0400

    improving responsive features

--- src/pages/dashboardPage.tsx
+++ src/pages/dashboardPage.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/pages/dashboardPage.css';
 
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 import { MdSettings, MdControlPoint } from 'react-icons/md';
 import { SiCashapp } from 'react-icons/si';
 import {  BiLogOut, BiWifi } from 'react-icons/bi';
 import { GiTap } from 'react-icons/gi';
-import { BsLightningFill } from 'react-icons/bs';
+import { BsLightningFill, BsX } from 'react-icons/bs';
 import { GiHamburgerMenu } from "react-icons/gi";
 
 
 
 function DashboardPage(){
     const {goBack} = useHistory()
+
+    const [sidebar, setSidebar] = useState(false)
+
+    function toggleStatus(){
+        setSidebar(!sidebar)
+    }
+
+    console.log(sidebar)
 
     return(
         <div className="container-dashboard">
@@ -42,10 +50,40 @@ function DashboardPage(){
                 </header>
                 <header className="mobile-header">
                     <div className="content">
-                        <p>JHCASH</p>
-                        <button>
-                            <GiHamburgerMenu size={56} color="#38D618"/>
-                        </button>
+                        <div className="navbar">
+                            <p className="title">JHCASH</p>
+                            <button onClick={toggleStatus}>
+                                <GiHamburgerMenu size={56} color="#38D618"  id={sidebar ? "hamburguer" : "hamburguer-active"}/>
+                                <BsX size={56} color="#38D618"  id={sidebar ? "x-button-active" : "x-button"}/>
+                            </button>
+                            
+                        </div>
+                        <nav className={ sidebar ? "nav-menu-active" : "nav-menu"}>
+                            <ul className="nav-menu-items">
+                                <li className="nav-toggle">
+                                    <Link to="#" className="menu-bars">
+                                        <MdSettings size={33} color="#fefefe"/>
+                                        <p className="text">Settings</p>
+                                    </Link>
+                                </li>
+                                <li className="nav-toggle">
+                                    <Link to="#" className="menu-bars">
+                                        <MdControlPoint size={33} color="#fefefe"/>
+                                        <p className="text">Add costs or earnings</p>
+                                    </Link>
+                                </li>
+                                <li className="nav-toggle">
+                                    <Link to="#" className="menu-bars">
+                                        <SiCashapp size={33} color="#fefefe"/>
+                                        <p className="text">Promotions on your area</p>
+                                    </Link>
+                                </li>
+                                <li className="out-item">
+                                    <button><BiLogOut size={43} color="#fefefe"/></button>
+                                </li>
+                            </ul>
+                        </nav>
+
                     </div>
                 </header>
                 <div className="content-area">
@@ -85,6 +123,33 @@ function DashboardPage(){
                             <div className="information">
                                 <header>Energy</header>
                                 <p>value: <strong>U$ 210,00</strong></p>
+                            </div>
+                        </div>
+                        <div className="debt-card3">
+                            <div className="ilustration-part">
+                                <BiWifi size={70} color="#fefefe"/>
+                            </div>
+                            <div className="information">
+                                <header>internet</header>
+                                <p>value: <strong>U$ 110,00</strong></p>
+                            </div>
+                        </div>
+                        <div className="debt-card3">
+                            <div className="ilustration-part">
+                                <BiWifi size={70} color="#fefefe"/>
+                            </div>
+                            <div className="information">
+                                <header>internet</header>
+                                <p>value: <strong>U$ 110,00</strong></p>
+                            </div>
+                        </div>
+                        <div className="debt-card3">
+                            <div className="ilustration-part">
+                                <BiWifi size={70} color="#fefefe"/>
+                            </div>
+                            <div className="information">
+                                <header>internet</header>
+                                <p>value: <strong>U$ 110,00</strong></p>
                             </div>
                         </div>
                         <div className="debt-card3">
